952160daa29267bd95982186a8de3b08616911dc
Chandan K <105306959+Chandan2485@users.noreply.github.com>
2024-02-26 05:20:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,362 @@
+let subject_arr = [
+  "Inquiry Regarding Guest Post Opportunities on Authority Sites",
+  "Exploring Guest Posting Opportunities for Your Brand",
+  "Exploring Collaborative Opportunities for Content Marketing and Brand Visibility",
+  "Strategic Blogger Engagement Solutions for SEO Professionals and Agencies",
+  "Amplifying SEO Impact: Specialized Blogger Outreach Services for Agencies",
+  "Elevate Your Online Presence: Tailored SEO Solutions and Expert Blogger Outreach Services",
+  "Blogger Outreach and Content Creation Services",
+  "Collaborative Opportunities in Blogger Outreach and Content Creation",
+  "Blogger Outreach and Content Collaboration Opportunities",
+  "Exploring Opportunities for Content Collaboration",
+  "Inquiry Regarding Potential Collaboration",
+  "Exploring Collaboration in the Content Marketing Sphere",
+  "Exploring Opportunities for Blogger Outreach Services",
+  "Exploring Blogger Outreach Collaboration Opportunities",
+  "Exploring Collaborative Opportunities in Content Amplification",
+  "Exploring Collaborative Opportunities for Quality Content Outreach",
+  "Exploring Opportunities for Content Collaboration and Outreach",
+  "Exploring Collaboration for Premium Content Placement",
+  "Unlocking Opportunities for Strategic Content Partnerships",
+  "Elevating Your Online Presence with Tailored Content Outreach",
+  "Crafting a Stronger Online Presence through Collaborative Content Placement",
+  "Amplifying Your Brand's Voice with Expert Content Outreach",
+  "Elevate Your Digital Presence with Specialized Content Collaboration",
+  "Strategic Content Collaboration for Enhanced Brand Presence",
+  "Elevating Your Online Presence through Strategic Content Syndication",
+  "Exploring Strategic Collaboration for Enhanced Digital Presence",
+  "Exploring Collaborative Opportunities for Digital Outreach and Content Placement",
+  "Exploring Opportunities for Enhanced Digital Engagement",
+];
+
+
+let thanks_note = [
+  "Best Regards,",
+  "Regards,",
+  "Thanks,",
+  "Thanks & Regards,",
+];
+
+
+let hello_note = [
+  'Hello Team,',
+  "Dear Team,",
+  "Hi There,"
+]
+
+
+let welcome = [
+  "I trust this email finds you in good health and spirits.",
+  "I trust this message finds you in good health and spirits.",
+  "I trust this email finds you well.",
+  "I hope this email finds you well.",
+  "Trust this message finds you in good health and spirits.",
+  "I trust this email finds you in good health and high spirits.",
+  "I trust this correspondence finds you in excellent health and positive spirits.",
+  "Trust this email finds you in good health.",
+  "I trust this message finds you in good health.",
+  "I hope this email finds you in good health.",
+  "Trust this message finds you in good health.",
+  "I trust this email finds you well and secure.",
+  "I trust this message finds you in good health and high spirits.",
+  "I trust this email finds you well and thriving.",
+  "I hope this email finds you in good health and high spirits.",
+  "I trust this message finds you well.",
+  "I trust this message finds you in good health and high spirits.",
+  "I Trust this message finds you well and secure.",
+  "I trust this email finds you well.",
+]
+
+
+
+let body = [
+  [
+    "I am Debasish, serving as an outreach executive at SEO Link Hub. Our company specializes in facilitating the acquisition of organic, contextually relevant backlinks from authentic bloggers. Additionally, we boast a proficient team of native writers hailing from the UK and USA, who are adept at crafting diverse articles tailored to your specific needs. Our writers ensure top-notch content, complete with images, outbound links, and meticulous formatting.",
+    "Should you be open to exploring guest post opportunities on esteemed platforms like *, and various others, kindly inform me at your earliest convenience. I will gladly furnish you with a personalized quotation based on your requirements.",
+    "I appreciate your valuable time and consideration."
+  ],
+  [
+    "I am Debasish, an outreach executive representing SEO Link Hub. Our company specializes in fostering genuine connections through the acquisition of high-quality, contextually fitting backlinks from credible bloggers. Additionally, we house a proficient team of native writers from the UK and USA, offering versatility in crafting articles to suit your varied needs. Our commitment includes delivering premium content complete with engaging visuals, outbound links, and impeccable formatting.",
+    "Should you be open to exploring guest post opportunities on reputable platforms like *, and many others, kindly express your interest, and I will promptly provide you with a bespoke quotation based on your unique requirements.",
+    "Thank you for considering our proposal. I look forward to the possibility of working together."
+  ],
+  [
+    "I am Debasish, a seasoned content marketing professional specializing in guest posting, link building, and crafting SEO-driven web content. At SEO Link Hub, we take pride in delivering top-notch, original content tailored for publication on authoritative platforms within your industry. Our focus is on providing you with high-quality content that not only aligns with your niche but also enhances your brand's authority and visibility through strategically placed links.",
+    "If the prospect of guest posts on influential sites such as *, and more appeals to you, kindly express your interest, and I will promptly share additional details.",
+    "Thank you for considering this collaboration opportunity. I appreciate your time and look forward to the possibility of working together."
+  ],
+  [
+    "I am writing to introduce myself, my name is Debasish and I specialize in blogger outreach at SEO Link Hub. We offer services to facilitate media outreach, securing guest posts, and obtaining backlinks from reputable sites. Some of the platforms we work with include noteworthy websites such as *, and many others.",
+    "Furthermore, I collaborate with a proficient team of native writers from both the UK and the USA. Our native writers are dedicated to producing high-quality content that includes engaging visuals, relevant outbound links, and meticulous formatting.",
+    "Should you have an interest in exploring these services, please do not hesitate to reach out. I would be happy to provide you with more detailed information.",
+    "Thank you for your valuable time and consideration."
+  ],
+  [
+    "My name is Debasish, and I specialize in fostering connections through blogger outreach. I am reaching out to explore potential collaborations in media outreach, securing guest posts, and acquiring backlinks from authoritative sites. Some of the platforms we work with include *, and various others.",
+    "Additionally, I have a proficient team of native writers based in both the UK and the USA. They excel in creating top-notch content, incorporating compelling visuals, relevant outbound links, and ensuring impeccable formatting.",
+    "Should you find our services intriguing, I would be delighted to provide you with more comprehensive details upon your request.",
+    "Thank you for your time and consideration."
+  ]
+]
+
+
+let array = [
+  "jpost.com",
+  "examiner.com",
+  "updato.com",
+  "androidguys.com",
+  "techtimes.com",
+  "streetinsider.com",
+  "deccanherald.com",
+  "ilounge.com",
+  "ipsnews.net",
+  "tribuneindia.com",
+  "globalbankingandfinance.com",
+  "tmcnet.com",
+  "freepressjournal.in",
+  "getintopc.com",
+  "theprint.in",
+  "menafn.com",
+  "widgetbox.com",
+  "israelnationalnews.com",
+  "eleven-magazine.com",
+  "tribuneonlineng.com",
+  "signalscv.com",
+  "beforeitsnews.com",
+  "theitbase.com",
+  "inspirationfeed.com",
+  "tgdaily.com",
+  "vegasnews.com",
+  "dodbuzz.com",
+  "fashiongonerogue.com",
+  "projectcubicle.com",
+  "thehollywoodnews.com",
+  "goodmenproject.com",
+  "artvoice.com",
+  "bbntimes.com",
+  "d-addicts.com",
+  "thefashionablehousewife.com",
+  "designscene.net",
+  "boomsbeat.com",
+  "architectureartdesigns.com",
+  "techdee.com",
+  "techclient.com",
+  "credihealth.com",
+  "fintechzoom.com",
+  "thelondoneconomic.com",
+  "trueactivist.com",
+  "natureworldnews.com",
+  "marketbusinessnews.com",
+  "nyasatimes.com",
+  "bitrebels.com",
+  "dailycal.org",
+  "thefashionisto.com",
+  "vbtcafe.com",
+  "enstarz.com",
+  "dailyiowan.com",
+  "inventiva.co.in",
+  "newsbreak.com",
+  "econotimes.com",
+  "markmeets.com",
+  "healthtian.com",
+  "thetechoutlook.com",
+  "maltatoday.com.mt",
+  "bignewsnetwork.com",
+  "latinpost.com",
+  "voice-online.co.uk",
+  "vcpost.com",
+  "gadgetstripe.com",
+  "tycoonstory.com",
+  "stylemotivation.com",
+  "techyeyes.com",
+  "weblizar.com",
+  "latinospost.com",
+  "greenprophet.com",
+  "ceoworld.biz",
+  "universityherald.com",
+  "earthweb.com",
+  "dailynewshungary.com",
+  "comfortskillz.com",
+  "wakingtimes.com",
+  "techstory.in",
+  "alltechbuzz.net",
+  "dashtech.org",
+  "samagame.com",
+  "hsewatch.com",
+  "worldofmedicalsaviours.com",
+  "techolac.com",
+  "techbullion.com",
+  "theyeshivaworld.com",
+  "nogentech.org",
+  "phoneswiki.com",
+  "smdailyjournal.com",
+  "webtechmantra.com",
+  "techshout.com",
+  "knowtechie.com",
+  "geeksaroundglobe.com",
+  "finsmes.com",
+  "healthsoothe.com",
+  "getdroidtips.com",
+  "state-journal.com",
+  "indiablooms.com",
+  "valiantceo.com",
+  "startupopinions.com",
+  "retail-insider.com",
+  "arrestyourdebt.com",
+  "nyweekly.com",
+  "doctorfolk.com",
+  "europeanbusinessreview.com",
+  "realwealthbusiness.com",
+  "scoopempire.com",
+  "voddler.com",
+  "startupxplore.com",
+  "world-today-news.com",
+  "tweakyourbiz.com",
+  "healthbenefitstimes.com",
+  "marketingdonut.co.uk",
+  "uniquenewsonline.com",
+  "marketresearchtelecast.com",
+  "apppicker.com",
+  "psychreg.org",
+  "theindianwire.com",
+  "theavtimes.com",
+  "taxguru.in",
+  "thebossmagazine.com",
+  "qrius.com",
+  "nenow.in",
+  "businessyield.com",
+  "bollyinside.com",
+  "naijatechguide.com",
+  "morebusiness.com",
+  "thesmallbusinessblog.net",
+  "businesswritingblog.com",
+  "worldfinancialreview.com",
+  "searates.com",
+  "computingforgeeks.com",
+  "latestmodapks.com",
+  "getproductkey.net",
+  "kenyanwallstreet.com",
+  "begindot.com",
+  "compsmag.com",
+  "techlila.com",
+  "tradebrains.in",
+  "qoshe.com",
+  "runnersblueprint.com",
+  "waybinary.com",
+  "sellerapp.com",
+  "technofizi.net",
+  "analyticssteps.com",
+  "easkme.com",
+  "tekedia.com",
+  "gearrice.com",
+  "hackerztrickz.com",
+  "shinesheets.com",
+  "wikitechy.com",
+  "biznakenya.com",
+  "lifeinsaudiarabia.net"
+];
+
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+
+
+function generate() {
+  if (document.getElementById("input").value.length > 3 ) {
+    if((document.getElementById("password").value == "123_Ck")){
+    let text = "";
+
+    for (let i = 0; i <= 10; i++) {
+      var randomNumber = Math.floor(Math.random() * 153);
+      console.log(randomNumber);
+      let x = array[randomNumber].split("")[0].toUpperCase()
+      let y = array[randomNumber].split("").slice(1).join("");
+      let z = x + y;
+
+      text += " " + z + ",";
+    }
+
+
+
+    var randomNumber_sub = Math.floor(Math.random() * 29);
+
+    var randomNumber_hello = Math.floor(Math.random() * 3);
+    var randomNumber_welcome = Math.floor(Math.random() * 19);
+    var randomNumber_div = Math.floor(Math.random() * 4);
+    var randomNumber_thanks = Math.floor(Math.random() * 4);
+
+
+    /*let input = document.getElementById("input");
+    let btn = document.getElementById("btn");*/
+    let section = document.querySelector("section");
+    section.innerHTML = "";
+
+
+    section.innerHTML += `<br><br><p>${subject_arr[randomNumber_sub]}</p><button id="btn1">Copy Subject</button><br><br>`;
+    section.innerHTML += `<button id="btn2">Copy Body</button> <br> <br>`;
+
+    section.innerHTML += `<div></div>`
+    section = document.querySelector("div");
+
+    section.innerHTML += `<p>${hello_note[randomNumber_hello]}</p>`;
+    section.innerHTML += `<p>${welcome[randomNumber_welcome]}</p>`;
+
+    body[randomNumber_div].forEach((item) => {
+      if (item.includes("*") && item.includes("Debasish")) {
+        let item_arr = item.split("Debasish");
+        item_arr = `${item_arr[0]} ${input.value} ${item_arr[1]}`;
+        item_arr = item_arr.split("*");
+        section.innerHTML += `<p>${item_arr[0]} <b>${text}</b> ${item_arr[1]}</p>`;
+      } else if (item.includes("*")) {
+
+        let item_arr = item.split("*");
+        section.innerHTML += `<p>${item_arr[0]}<b>${text}</b>${item_arr[1]}</p>`
+      } else if (item.includes("Debasish")) {
+
+        let item_arr = item.split("Debasish");
+        section.innerHTML += `<p>${item_arr[0]}${input.value.trim()}${item_arr[1]}</p>`
+      } else {
+        section.innerHTML += `<p>${item}</p>`
+      }
+    })
+
+    section.innerHTML += `<p>${thanks_note[randomNumber_thanks]}</p>`;
+    section.innerHTML += `<p>${input.value}</p>`;
+    console.log(section.innerText);
+    let textareaSubject= document.querySelector("p");
+    let textareaBody = document.querySelector("div");
+    section.innerHTML += `<br><br><br><br><br><br><br><br><textarea name="" id="1" cols="175" rows="10">${textareaBody.innerText}</textarea>`;
+    section.innerHTML += `<textarea name="" id="2" cols="175" rows="1">${textareaSubject.innerText}</textarea>`;
+  }else{
+    let section = document.querySelector("section");
+    section.innerHTML = `<H1>Enter Your Password</H1>`
+  }
+  } else {
+    let section = document.querySelector("section");
+    section.innerHTML = `<H1>Enter Your Name</H1>`
+  }
+
+
+  function copyEmail() {
+    let textarea1 = document.getElementById("1");
+    textarea1.select();
+    navigator.clipboard.writeText(textarea1.value);
+  }
+
+  function copySubject() {
+    let textarea2 = document.getElementById("2");
+    textarea2.select();
+    navigator.clipboard.writeText(textarea2.value);
+  }
+  let btn1 = document.getElementById("btn1");
+  let btn2 = document.getElementById("btn2");
+
+  btn1.addEventListener("click", copySubject);
+  btn2.addEventListener("click", copyEmail);
+}
+
+
+
+
+
+btn.addEventListener("click", generate)
+
